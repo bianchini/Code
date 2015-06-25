e8a72914cd0f0eac2b94302bd7e8790383a24277
Lorenzo <lore.bianchini@gmail.com>
2015-06-25 12:30:02 +0200

    added more variables

--- test/btag_analyzer.cpp
+++ test/btag_analyzer.cpp
@@ -37,7 +37,20 @@ int main(int argc, char *argv[]){
   float csv_rnd_4   [99];
   float csv_inp_4   [99];
 
+  float corr_rnd_0   [8];
+  float corr_inp_0   [8];
+  float corr_rnd_1   [8];
+  float corr_inp_1   [8];
+  float corr_rnd_2   [8];
+  float corr_inp_2   [8];
+  float corr_rnd_3   [8];
+  float corr_inp_3   [8];
+  float corr_rnd_4   [8];
+  float corr_inp_4   [8];
+
+  float HT, met;
   float pt    [99];
+  float eta   [99];
   int   pdgid [99];
 
   map<int, float*> csv_rnd_map;
@@ -53,6 +66,20 @@ int main(int argc, char *argv[]){
   csv_inp_map[4] = csv_inp_4;
   csv_rnd_map[4] = csv_rnd_4;
 
+  map<int, float*> corr_rnd_map;
+  map<int, float*> corr_inp_map;
+  corr_inp_map[0] = corr_inp_0;
+  corr_rnd_map[0] = corr_rnd_0;
+  corr_inp_map[1] = corr_inp_1;
+  corr_rnd_map[1] = corr_rnd_1;
+  corr_inp_map[2] = corr_inp_2;
+  corr_rnd_map[2] = corr_rnd_2;
+  corr_inp_map[3] = corr_inp_3;
+  corr_rnd_map[3] = corr_rnd_3;
+  corr_inp_map[4] = corr_inp_4;
+  corr_rnd_map[4] = corr_rnd_4;
+
+
   tout->Branch("event",    &event,    "event/I");
   tout->Branch("njet",     &njet,     "njet/I");
   tout->Branch("ncat",     &ncat,     "ncat/I");
@@ -60,8 +87,11 @@ int main(int argc, char *argv[]){
   tout->Branch("pcat",     pcat,      "pcat[ncat]/F");
   tout->Branch("pass",     pass,      "pass[ncat]/I");
   tout->Branch("pass_rnd", pass_rnd,  "pass_rnd[ncat]/I");
-  tout->Branch("pt",       pt,        "pcat[njet]/F");
+  tout->Branch("pt",       pt,        "pt[njet]/F");
+  tout->Branch("eta",      eta,       "eta[njet]/F");
   tout->Branch("pdgid",    pdgid,     "pdgid[njet]/I");
+  tout->Branch("HT",      &HT,      "HT/F");
+  tout->Branch("met",     &met,     "met/F");
 
   for(auto it = csv_rnd_map.begin() ; it!=csv_rnd_map.end(); ++it)
     tout->Branch(Form("csv_rnd_%d",it->first),  it->second,   Form("csv_rnd_%d[njet]/F",it->first));
@@ -69,8 +99,14 @@ int main(int argc, char *argv[]){
   for(auto it = csv_inp_map.begin() ; it!=csv_inp_map.end(); ++it)
     tout->Branch(Form("csv_inp_%d",it->first),  it->second,   Form("csv_inp_%d[njet]/F",it->first));
 
+  for(auto it = corr_rnd_map.begin() ; it!=corr_rnd_map.end(); ++it)
+    tout->Branch(Form("corr_rnd_%d",it->first),  it->second,   Form("corr_rnd_%d[8]/F",it->first));
 
-  TFile* f = TFile::Open("../MEAnalysis/root/ControlPlotsV6.root","READ");
+  for(auto it = corr_inp_map.begin() ; it!=corr_inp_map.end(); ++it)
+    tout->Branch(Form("corr_inp_%d",it->first),  it->second,   Form("corr_inp_%d[8]/F",it->first));
+
+
+  TFile* f = TFile::Open("../MEAnalysis/root/ControlPlotsV6_finerPt.root","READ");
   TH3D* h3_b = (TH3D*)f->Get("csv_b_pt_eta");
   TH3D* h3_c = (TH3D*)f->Get("csv_c_pt_eta");
   TH3D* h3_l = (TH3D*)f->Get("csv_l_pt_eta");
@@ -108,6 +144,7 @@ int main(int argc, char *argv[]){
   double jets_mcPt   [99];
   int jets_mcFlavour [99];
   double btag_LR_4b_2b;
+  double met_pt;
 
   tin->SetBranchAddress("evt",         &evt);
   tin->SetBranchAddress("njets",       &njets);
@@ -121,6 +158,7 @@ int main(int argc, char *argv[]){
   tin->SetBranchAddress("jets_mcPt",   jets_mcPt   );
   tin->SetBranchAddress("jets_mcFlavour",  jets_mcFlavour   );
   tin->SetBranchAddress("btag_LR_4b_2b",   &btag_LR_4b_2b);
+  tin->SetBranchAddress("met_pt",   &met_pt);
 
   Long64_t nentries = tin->GetEntries();
   cout << "Total entries: " << nentries << endl;
@@ -141,9 +179,14 @@ int main(int argc, char *argv[]){
     vector<JetCategory> cats;
     vector<Object*> objects_mem;
     vector<JetCategory*> cats_mem;
+
+    HT  = 0.;
+    met = met_pt; 
     for( int j = 0 ; j < njets ; ++j){
 
       pt[j]    = jets_pt[j];
+      HT      += jets_pt[j];
+      eta[j]   = jets_eta[j];
       pdgid[j] = jets_mcFlavour[j];
 
       TLorentzVector lv;
@@ -175,12 +218,11 @@ int main(int argc, char *argv[]){
       cats_mem.push_back( cat );
     }
 
-
-
     event = evt;
-    njet = njets;
-    ncat = int(cats.size());    
-    ntag = int(nBCSVM);
+    njet  = njets;
+    ncat  = int(cats.size());    
+    ntag  = int(nBCSVM);
+    
 
     //cout << "\tRunning BTagRandomizerOutput with " << cats.size() << " confs" << endl;
     vector<BTagRandomizerOutput> out_all = rnd->run_all( cats );
@@ -195,6 +237,97 @@ int main(int argc, char *argv[]){
 	(csv_inp_map.at(o))[j] = (out_all[o].input_btag)[j];
       }
 
+      //// Input
+      float dR_min_bb =  99.;
+      float dR_min_jj =  99.;
+      float dR_max_bb = -99.;
+      float dR_max_jj = -99.;
+      float m_min_bb  =  99.;
+      float m_min_jj  =  99.;
+      float m_max_bb  = -99.;
+      float m_max_jj  = -99.;
+      for( int j = 0 ; j < njets-1 ; ++j){
+	TLorentzVector lv_j;
+	lv_j.SetPtEtaPhiM(jets_pt[j], jets_eta[j], jets_phi[j], jets_mass[j]);
+	int tag_j= (out_all[o].input_btag)[j]>csv_cut;
+	for( int k = j+1 ; k < njets ; ++k){
+	  TLorentzVector lv_k;
+	  lv_k.SetPtEtaPhiM(jets_pt[k], jets_eta[k], jets_phi[k], jets_mass[k]);	
+	  int tag_k = (out_all[o].input_btag)[k]>csv_cut;
+	  
+	  float dR = lv_j.DeltaR( lv_k );
+	  float m  = (lv_k+lv_j).M();
+	  
+	  if( tag_j && tag_k ){
+	    if( dR <= dR_min_bb ) dR_min_bb = dR;
+	    if( m  <= m_min_bb  ) m_min_bb  = m;
+	    if( dR >= dR_max_bb ) dR_max_bb = dR;
+	    if( m  >= m_max_bb  ) m_max_bb  = m;
+	  }
+	  if( !tag_j && !tag_k ){
+	    if( dR <= dR_min_jj ) dR_min_jj = dR;
+	    if( m  <= m_min_jj  ) m_min_jj  = m;
+	    if( dR >= dR_max_jj ) dR_max_jj = dR;
+	    if( m  >= m_max_jj  ) m_max_jj  = m;
+	  }
+	  
+	}
+      }
+      
+      (corr_inp_map.at(o))[0] = dR_min_bb;
+      (corr_inp_map.at(o))[1] = dR_min_jj;
+      (corr_inp_map.at(o))[2] = dR_max_bb;
+      (corr_inp_map.at(o))[3] = dR_max_jj;
+      (corr_inp_map.at(o))[4] = m_min_bb;
+      (corr_inp_map.at(o))[5] = m_min_jj;
+      (corr_inp_map.at(o))[6] = m_max_bb;
+      (corr_inp_map.at(o))[7] = m_max_jj;
+
+      ///////// Rand
+      dR_min_bb =  99.;
+      dR_min_jj =  99.;
+      dR_max_bb = -99.;
+      dR_max_jj = -99.;
+      m_min_bb  =  99.;
+      m_min_jj  =  99.;
+      m_max_bb  = -99.;
+      m_max_jj  = -99.;
+      for( int j = 0 ; j < njets-1 ; ++j){
+	TLorentzVector lv_j;
+	lv_j.SetPtEtaPhiM(jets_pt[j], jets_eta[j], jets_phi[j], jets_mass[j]);
+	int tag_j = (out_all[o].rnd_btag)[j]>csv_cut;
+	for( int k = j+1 ; k < njets ; ++k){
+	  TLorentzVector lv_k;
+	  lv_k.SetPtEtaPhiM(jets_pt[k], jets_eta[k], jets_phi[k], jets_mass[k]);	
+	  int tag_k = (out_all[o].rnd_btag)[k]>csv_cut;
+	  
+	  float dR = lv_j.DeltaR( lv_k );
+	  float m  = (lv_k+lv_j).M();
+	  
+	  if( tag_j && tag_k ){
+	    if( dR <= dR_min_bb ) dR_min_bb = dR;
+	    if( m  <= m_min_bb  ) m_min_bb  = m;
+	    if( dR >= dR_max_bb ) dR_max_bb = dR;
+	    if( m  >= m_max_bb  ) m_max_bb  = m;
+	  }
+	  if( !tag_j && !tag_k ){
+	    if( dR <= dR_min_jj ) dR_min_jj = dR;
+	    if( m  <= m_min_jj  ) m_min_jj  = m;
+	    if( dR >= dR_max_jj ) dR_max_jj = dR;
+	    if( m  >= m_max_jj  ) m_max_jj  = m;
+	  }
+	  
+	}
+      }
+      
+      (corr_rnd_map.at(o))[0] = dR_min_bb;
+      (corr_rnd_map.at(o))[1] = dR_min_jj;
+      (corr_rnd_map.at(o))[2] = dR_max_bb;
+      (corr_rnd_map.at(o))[3] = dR_max_jj;
+      (corr_rnd_map.at(o))[4] = m_min_bb;
+      (corr_rnd_map.at(o))[5] = m_min_jj;
+      (corr_rnd_map.at(o))[6] = m_max_bb;
+      (corr_rnd_map.at(o))[7] = m_max_jj;
     }
 
 
--- test/validate.C
+++ test/validate.C
@@ -4,14 +4,18 @@
 
   //TString name = "tth_13tev_amcatnlo_pu20bx25_0";
   //TString name = "tth_13tev_amcatnlo_pu20bx25_1";
-  //TString name = "ttjets_13tev_madgraph_pu20bx25_phys14_0";
-  TString name = "ttjets_13tev_madgraph_pu20bx25_phys14_1";
+  TString name = "ttjets_13tev_madgraph_pu20bx25_phys14_0";
+  //TString name = "ttjets_13tev_madgraph_pu20bx25_phys14_1";
 
   TFile* fout = new TFile("test/validate_"+name+".root","RECREATE");
   fout->mkdir("csv");
   fout->mkdir("csv_ratio");
   fout->mkdir("pt");
   fout->mkdir("pt_ratio");
+  fout->mkdir("dR");
+  fout->mkdir("dR_ratio");
+  fout->mkdir("Mjj");
+  fout->mkdir("Mjj_ratio");
   
   TFile* f = TFile::Open("test/btag_out_"+name+".root");
   TTree* t = (TTree*)f->Get("tree");
@@ -21,19 +25,92 @@
   for( int njet = 4 ; njet <=6 ; ++njet ){
     for( int i = 0 ; i <=4 ; ++i ){
       h->Reset();
+      h->Sumw2();
       t->Draw("njet>>h",Form("njet==%d && pass[%d]",njet,i));
-      float pass = h->Integral(); 
+      double pass_err=0.; 
+      float pass = h->IntegralAndError(1, h->GetNbinsX(), pass_err); 
       
       h->Reset();
       t->Draw("njet>>h",Form("(njet==%d)*pcat[%d]", njet,i));
-      float weight = h->Integral(); 
+      double weight_err=0.; 
+      float weight = h->IntegralAndError(1, h->GetNbinsX(), weight_err); 
 
       float ratio = pass>0 ? (weight-pass)/pass  : 0.;
       printf("Jet: %d, Tag: %d\n", njet, i);
-      printf("\tPASS:   %.0f +/- %.0f\n", pass, sqrt(pass));
-      printf("\tWEIGHT: %.0f\n", weight);
+      printf("\tPASS:   %.0f +/- %.1f\n", pass, pass_err);
+      printf("\tWEIGHT: %.0f +/- %.1f\n", weight, weight_err);
       printf("\tDiff: %.1f\%\n",   ratio*100);
-  
+
+      for(int k = 0 ; k <4 ; ++k){
+	string title = "";
+	if(k==0) title = "min for tagged jets";
+	if(k==1) title = "min for untagged jets";
+	if(k==2) title = "max for tagged jets";
+	if(k==3) title = "max for untagged jets";
+
+	TH1F* h_inp   = new TH1F(Form("h_inp_%d_%d_%d",  njet,i,k),Form("N_{jet}=%d, N_{tag}=%d; #DeltaR %s", njet, i,title.c_str()),20,0,6);
+	TH1F* h_rnd   = new TH1F(Form("h_rnd_%d_%d_%d",  njet,i,k),Form("N_{jet}=%d, N_{tag}=%d; #DeltaR %s", njet, i,title.c_str()),20,0,6);
+	TH1F* h_ratio = new TH1F(Form("h_ratio_%d_%d_%d",njet,i,k),Form("N_{jet}=%d, N_{tag}=%d; #DeltaR %s", njet, i,title.c_str()),20,0,6);
+	h_inp->Sumw2();
+	h_rnd->Sumw2();
+	h_ratio->Sumw2();
+	t->Draw(Form("corr_inp_%d[%d]>>h_inp_%d_%d_%d",i, k, njet, i,k), Form("njet==%d && pass[%d]",njet,i));
+	t->Draw(Form("corr_rnd_%d[%d]>>h_rnd_%d_%d_%d",i, k, njet, i,k), Form("(njet==%d)*pcat[%d]", njet,i));
+	fout->cd();            
+	//for( int b = 1 ; b <= h_ratio->GetNbinsX() ; ++b ){
+	//  float x =  (h_rnd->GetBinContent(b)>0.) ? h_rnd->GetBinError(b)/h_rnd->GetBinContent(b) : 0.  ;
+	//  float y =  (h_inp->GetBinContent(b)>0.) ? h_inp->GetBinError(b)/h_inp->GetBinContent(b) : 0.  ;
+	//  h_ratio->SetBinError(b,  h_ratio->GetBinContent(b)*sqrt( x*x + y*y) );
+	//}
+
+	h_inp->SetLineColor(kBlue);
+	h_rnd->SetLineColor(kRed);
+	h_ratio->SetLineColor(kBlack);      
+	fout->cd("dR");
+	h_inp->Write();
+	h_rnd->Write();
+	fout->cd("dR_ratio");
+	h_rnd->Add(h_inp, -1.);
+	h_ratio->Divide(h_rnd,h_inp,1.0,1.0);
+	h_ratio->Write();
+	delete h_inp; delete h_rnd; delete h_ratio;
+      }
+
+      for(int k = 4 ; k <8 ; ++k){
+	string title = "";
+	if(k==4) title = "min for tagged jets";
+	if(k==5) title = "min for untagged jets";
+	if(k==6) title = "max for tagged jets";
+	if(k==7) title = "max for untagged jets";
+
+	TH1F* h_inp   = new TH1F(Form("h_inp_%d_%d_%d",  njet,i,k),Form("N_{jet}=%d, N_{tag}=%d; dijet mass %s", njet, i,title.c_str()),20,0, k<6 ? 200 : 600);
+	TH1F* h_rnd   = new TH1F(Form("h_rnd_%d_%d_%d",  njet,i,k),Form("N_{jet}=%d, N_{tag}=%d; dijet mass %s", njet, i,title.c_str()),20,0, k<6 ? 200 : 600);
+	TH1F* h_ratio = new TH1F(Form("h_ratio_%d_%d_%d",njet,i,k),Form("N_{jet}=%d, N_{tag}=%d; dijet mass %s", njet, i,title.c_str()),20,0, k<6 ? 200 : 600);
+	h_inp->Sumw2();
+	h_rnd->Sumw2();
+	h_ratio->Sumw2();
+	t->Draw(Form("corr_inp_%d[%d]>>h_inp_%d_%d_%d",i, k, njet, i,k), Form("njet==%d && pass[%d]",njet,i));
+	t->Draw(Form("corr_rnd_%d[%d]>>h_rnd_%d_%d_%d",i, k, njet, i,k), Form("(njet==%d)*pcat[%d]", njet,i));
+	fout->cd();            
+	//for( int b = 1 ; b <= h_ratio->GetNbinsX() ; ++b ){
+	//  float x =  (h_rnd->GetBinContent(b)>0.) ? h_rnd->GetBinError(b)/h_rnd->GetBinContent(b) : 0.  ;
+	//  float y =  (h_inp->GetBinContent(b)>0.) ? h_inp->GetBinError(b)/h_inp->GetBinContent(b) : 0.  ;
+	//  h_ratio->SetBinError(b,  h_ratio->GetBinContent(b)*sqrt( x*x + y*y) );
+	//}
+
+	h_inp->SetLineColor(kBlue);
+	h_rnd->SetLineColor(kRed);
+	h_ratio->SetLineColor(kBlack);      
+	fout->cd("Mjj");
+	h_inp->Write();
+	h_rnd->Write();
+	fout->cd("Mjj_ratio");
+	h_rnd->Add(h_inp, -1.);
+	h_ratio->Divide(h_rnd,h_inp,1.0,1.0);
+	h_ratio->Write();
+	delete h_inp; delete h_rnd; delete h_ratio;
+      }
+
       for(int k = 0 ; k <6 ; ++k){
 	if(k>=njet) continue;
 	TH1F* h_csv_inp   = new TH1F(Form("h_csv_inp_%d_%d_%d",  njet,i,k),Form("N_{jet}=%d, N_{tag}=%d, Jet %d; CSV", njet, i, k),10,0,1);
@@ -57,11 +134,9 @@
 	t->Draw(Form("pt[%d]>>h_pt_rnd_%d_%d_%d",k,njet, i,k), Form("(njet==%d)*pcat[%d]", njet,i));
 
 	fout->cd();            
-	h_csv_ratio->Divide(h_csv_rnd,h_csv_inp,1.0,1.0);
 	h_csv_inp->SetLineColor(kBlue);
 	h_csv_rnd->SetLineColor(kRed);
 	h_csv_ratio->SetLineColor(kBlack);
-	h_pt_ratio->Divide(h_pt_rnd,h_pt_inp,1.0,1.0);
 	h_pt_inp->SetLineColor(kBlue);
 	h_pt_rnd->SetLineColor(kRed);
 	h_pt_ratio->SetLineColor(kBlack);
@@ -75,13 +150,18 @@
 	h_csv_rnd->Write();
 
 	fout->cd("csv_ratio");
+	h_csv_rnd->Add(h_csv_inp,-1.);
+	h_csv_ratio->Divide(h_csv_rnd,h_csv_inp,1.0,1.0);
 	h_csv_ratio->Write();
 
 	fout->cd("pt_ratio");
+	h_pt_rnd->Add(h_pt_inp,-1.);
+	h_pt_ratio->Divide(h_pt_rnd,h_pt_inp,1.0,1.0);
 	h_pt_ratio->Write();
 	
 	delete h_pt_inp; delete h_pt_rnd; delete h_pt_ratio;
 	delete h_csv_inp; delete h_csv_rnd; delete h_csv_ratio;
+
 	f->cd();
       }
     }
