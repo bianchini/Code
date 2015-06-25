1c8a486b13f76c51d5749f129f53dd0ce002aef0
Joosep Pata <joosep.pata@cern.ch>
2015-06-25 12:11:14 +0300

    change pdf set

--- src/classes.h
+++ src/classes.h
@@ -1,4 +1,5 @@
 #include "TTH/MEIntegratorStandalone/interface/Integrand.h"
+#include "TTH/MEIntegratorStandalone/interface/BTagRandomizer.h"
 
 //template class map<string, vector<Algo::Decay::Decay> >;
 //template class vector<Algo::Decay::Decay>;
